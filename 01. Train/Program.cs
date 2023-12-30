e68ad6c91eaa923caf96d127c1af41b2a46a33fc
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-12-30 12:26:09 +0200

    Add files via upload

---
+++ 01. Train/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            string input;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] cmdArgs = input
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmdArgs.Length > 1)
+                {
+                    wagons.Add(int.Parse(cmdArgs[1]));
+                    continue;
+                }
+                else
+                {
+                    int incomingPassengers = int.Parse(cmdArgs[0]);
+
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if ((maxCapacity - wagons[i]) >= incomingPassengers)
+                        {
+                            wagons[i] += incomingPassengers;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
