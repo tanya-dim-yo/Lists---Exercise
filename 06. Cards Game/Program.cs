31f38b56d78bfe17ad7d0735e4d091178521221d
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-04 01:43:18 +0200

    Add files via upload

---
+++ 06. Cards Game/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _06.Cards_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstPlayer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondPlayer = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            while (firstPlayer.Count > 0 && secondPlayer.Count > 0)
+            {
+                if (firstPlayer[0] > secondPlayer[0])
+                {
+                    firstPlayer.Add(secondPlayer[0]);
+                    secondPlayer.RemoveAt(0);
+
+                    int winningCard = firstPlayer[0];
+                    firstPlayer.RemoveAt(0);
+                    firstPlayer.Add(winningCard);
+                    continue;
+                }
+                else if (firstPlayer[0] < secondPlayer[0])
+                {
+                    secondPlayer.Add(firstPlayer[0]);
+                    firstPlayer.RemoveAt(0);
+
+                    int winningCard = secondPlayer[0];
+                    secondPlayer.RemoveAt(0);
+                    secondPlayer.Add(winningCard);
+                    continue;
+                }
+                else if (firstPlayer[0] == secondPlayer[0])
+                {
+                    firstPlayer.RemoveAt(0);
+                    secondPlayer.RemoveAt(0);
+                    continue;
+                }
+            }
+
+            if (firstPlayer.Count > 0)
+            {
+                Console.WriteLine($"First player wins! Sum: {firstPlayer.Sum()}");
+            }
+            else if (secondPlayer.Count > 0)
+            {
+                Console.WriteLine($"Second player wins! Sum: {secondPlayer.Sum()}");
+            }
+        }
+    }
+}
