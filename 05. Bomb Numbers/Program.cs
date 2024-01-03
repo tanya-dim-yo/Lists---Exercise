e1bdaaf9e5ebfe988425d01e57f0a8fcf09be6df
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-03 04:07:15 +0200

    Add files via upload

---
+++ 05. Bomb Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _05.Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> bombInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int bomb = bombInfo[0];
+            int power = bombInfo[1];
+
+            while (numbers.Contains(bomb))
+            {
+                int bombIndex = numbers.IndexOf(bomb);
+                int startIndex = bombIndex - power;
+
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                int endIndex = bombIndex + power;
+
+                if (endIndex > numbers.Count - 1)
+                {
+                    endIndex = numbers.Count - 1;
+                }
+
+                numbers.RemoveRange(startIndex, endIndex - startIndex + 1);
+            }
+
+            int sum = 0;
+
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
