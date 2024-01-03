d6d5d58000a372cecdfbfd0f0833cd89b341e158
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2024-01-04 01:44:04 +0200

    Add files via upload

---
+++ 07. Append Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace _07.Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split("|", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<int> output = new List<int>();
+
+            for (int i = input.Count - 1; i >= 0; i--)
+            {
+                string current = input[i];
+                List<int> currentString = current
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                foreach (int currentInt in currentString)
+                {
+                    output.Add(currentInt);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", output));
+        }
+    }
+}
